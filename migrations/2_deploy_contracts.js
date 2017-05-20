5a0b2dfc6efdd70cb133f11034448bb91c7af796
mvpratt <mvpratt@gmail.com>
2017-05-20 16:02:14 -0600

    added support for geth in truffle.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,3 @@
 module.exports = function(deployer) {
-
   deployer.deploy(FundingHub);
-  
 };
--- truffle.js
+++ truffle.js
@@ -12,5 +12,28 @@ module.exports = {
   rpc: {
     host: "localhost",
     port: 8545
+  },
+  networks: {
+//  "live": {
+//    network_id: 1, // Ethereum public network
+    // optional config values
+    // host - defaults to "localhost"
+    // port - defaults to 8545
+    // gas
+    // gasPrice
+    // from - default address to use for any transaction Truffle makes during migrations
+//  },
+//  "morden": {
+//    network_id: 2,        // Official Ethereum test network
+//    host: "178.25.19.88", // Random IP for example purposes (do not use)
+//    port: 80
+//  },
+  "staging": {
+    network_id: 42 // custom private network
+    // use default rpc settings
+  },
+  "development": {
+    network_id: "default"
   }
+}
 };
