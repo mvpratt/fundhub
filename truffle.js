8933236019305268f060ca457ee165c107fea272
mvpratt <mvpratt@gmail.com>
2017-05-21 14:43:22 -0600

    Completed basic testing on geth.  Had to use this command:  “truffle build --network staging; truffle migrate --network staging”

--- truffle.js
+++ truffle.js
@@ -14,26 +14,12 @@ module.exports = {
     port: 8545
   },
   networks: {
-//  "live": {
-//    network_id: 1, // Ethereum public network
-    // optional config values
-    // host - defaults to "localhost"
-    // port - defaults to 8545
-    // gas
-    // gasPrice
-    // from - default address to use for any transaction Truffle makes during migrations
-//  },
-//  "morden": {
-//    network_id: 2,        // Official Ethereum test network
-//    host: "178.25.19.88", // Random IP for example purposes (do not use)
-//    port: 80
-//  },
-  "staging": {
-    network_id: 42 // custom private network
-    // use default rpc settings
-  },
-  "development": {
-    network_id: "default"
+    "staging": {
+      network_id: 42 // custom private network
+      // use default rpc settings
+    },
+    "development": {
+      network_id: "default"
+    }
   }
-}
 };
