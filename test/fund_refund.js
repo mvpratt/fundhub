f066b3e4cb06e04b6a1007c8e631c0ea0948d546
mvpratt <mvpratt@gmail.com>
2017-06-02 10:43:25 -0600

    modularized createProject function in refund test

--- test/fund_refund.js
+++ test/fund_refund.js
@@ -38,73 +38,53 @@ request refund - allowed
 
   it("Project version should match", function(done) {
 
-  var proj_index = 1;
-  var user_addr = alice;
-  var amount_contribute = web3.toWei(1, "ether");
-  var amount_goal = web3.toWei(10, "ether");
-  var duration = 50;    
-
-    FundingHub.new({from: coinbase})
-    .then(function(fundhub) {
-        fundhub.createProject(user_addr, amount_goal, duration, {from: user_addr, gas: 4500000})
-        .then(function(){
-          return fundhub.getNumProjects.call();
+  createProject()
+        .then(function(myproj){
+          return myproj.getVersion.call();
         })
-        .then(function(num){
-          console.log("num projects " + num);
-          return fundhub.getProjectAddress.call(proj_index);
-        })
-        .then(function(addr){
-            return Project.at(addr);
-          })
-        .then(function(instance){
-            proj = instance;  
-            return proj.getVersion.call();
-          })
         .then(function(version){
-            assert.equal(version, 2, "Version doesn't match!"); 
-            done();
+          assert.equal(version.valueOf(), 2, "Version doesn't match!"); 
+          done();
         })
         .catch(done);
-    }).catch(done);
   });
+
 
 
 function createProject(){
 
-  var proj_index = 1;
   var user_addr = alice;
   var amount_contribute = web3.toWei(1, "ether");
   var amount_goal = web3.toWei(10, "ether");
   var duration = 50;
-  var proj;    
+  var proj;
+  var fundhub;    
 
   return new Promise(function(resolve,reject){
 
     FundingHub.new({from: coinbase})
-        .then(function(fundhub) {
+        .then(function(instance) {
+          fundhub = instance;
           fundhub.createProject(user_addr, amount_goal, duration, {from: user_addr, gas: 4500000});
         })  
         .then(function(){
           return fundhub.getNumProjects.call();
         })
         .then(function(num){
-          console.log("num projects " + num);
-          return fundhub.getProjectAddress.call(proj_index);
+          //console.log("num projects " + num);
+          return fundhub.getProjectAddress.call(num);
         })
         .then(function(addr){
-            return Project.at(addr);
+          //console.log("address: "+addr);
+          return Project.at(addr);
         })
-        .then(function(instance){
-            proj = instance;  
-            return proj.getVersion.call();
-        })
+        .then(function(proj){
+          resolve(proj);
+          })
         .catch(function(e) {
           console.log(e);
-          setStatus("Error creating project; see log.");
         });
 
-    resolve(proj);
   });
 } // function
 
